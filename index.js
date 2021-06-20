9242d9b31b3b750ea2949a6e3245e92a6bea7866
alexey-dc <alexey.cad@gmail.com>
2021-06-20 17:45:16 -0400

    await on server start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const Server = require("./app/server")
 
 const begin = async () => {
-  new Server(process.env.EXPRESS_PORT).start()
+  await new Server(process.env.EXPRESS_PORT).start()
   console.log(`Server running in --- ${process.env.NODE_ENV} --- on port ${process.env.EXPRESS_PORT}`)
 }
 
