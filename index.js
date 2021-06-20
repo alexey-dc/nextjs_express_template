0bdee0b75040e3cb49377db76b80ee13ea02547d
alexey-dc <alexey.cad@gmail.com>
2021-06-20 16:54:20 -0400

    Add environment printout to ready message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Server = require("./app/server")
 
 const begin = async () => {
   new Server(process.env.EXPRESS_PORT).start()
-  console.log(`Server running on port ${process.env.EXPRESS_PORT}`)
+  console.log(`Server running in --- ${process.env.NODE_ENV} --- on port ${process.env.EXPRESS_PORT}`)
 }
 
 begin()
