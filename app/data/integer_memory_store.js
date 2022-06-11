6b9826a53ccbe0a1413057c44261c755924e7db3
alexey-dc <alexey.cad@gmail.com>
2022-06-10 23:41:08 -0400

    Safer API-side incr/decr

--- app/data/integer_memory_store.js
+++ app/data/integer_memory_store.js
@@ -4,11 +4,18 @@ class InMemoryStore {
   }
 
   incr() {
-    this._value++
+    this._value = (this._value + 1) % 100000
   }
 
   decr() {
     this._value--
+    /*
+      https://web.archive.org/web/20090717035140if_/javascript.about.com/od/problemsolving/a/modulobug.htm
+      https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
+    */
+    if(this._value < -100000) {
+      this._value = 0
+    }
   }
 
   get value() {
